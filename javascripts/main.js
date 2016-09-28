2f48b58f8786eedd37d841502589f564aa2ac4af
salt9k <james.s.sharpe@gmail.com>
2016-09-28 16:34:26 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,3 +14,30 @@ function initMap(){
           map: map
         });
 }
+
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+function calcRoute(){
+        var request = {
+                origin: "200 N Gary Ave Roselle, IL 60172",
+                destination: "Dekalb, IL",
+                travelMode: DRIVING,
+                //transitOptions: TransitOptions,
+                //drivingOptions: DrivingOptions,
+                //unitSystem: UnitSystem,
+                //waypoints[]: DirectionsWaypoint,
+                //optimizeWaypoints: Boolean,
+                //provideRouteAlternatives: Boolean,
+                //avoidHighways: Boolean,
+                //avoidTolls: Boolean,
+                //region: String
+        };
+directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
+        
+}
