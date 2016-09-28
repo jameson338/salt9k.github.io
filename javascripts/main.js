183c9b0b503b4706d3fdb6e78e256d73ed3cf88a
salt9k <james.s.sharpe@gmail.com>
2016-09-28 15:02:53 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,11 @@
 function initMap(){
-        var uluru = {lat: -25.363, lng: 131.044};
+        var roselle = {lat: 41.984, lng: 88.079};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: uluru
         });
         var marker = new google.maps.Marker({
-          position: uluru,
+          position: roselle,
           map: map
         });
 }
