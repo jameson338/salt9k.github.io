bed91d1ba6b15d044ace4f716eb6c5b264e9dbd1
salt9k <james.s.sharpe@gmail.com>
2016-09-28 15:23:43 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,16 @@
 function initMap(){
         var roselle = {lat: 41.986, lng: -88.122};
+        var elgin = {lat: 42.035, lng: -88.281};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 10,
           center: roselle
         });
         var marker = new google.maps.Marker({
           position: roselle,
+          map: map
+        });
+        var marker2 = new google.maps.Marker({
+          position: elgin,
           map: map
         });
 }
